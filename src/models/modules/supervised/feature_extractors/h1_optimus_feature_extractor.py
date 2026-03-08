611ba4c9b3890688a5eacd21c672ccadc384ef1f
Parham Bateni <parham@c-002-001-005.client.tudelft.eduvpn.nl>
2026-03-08 15:11:46 +0100

    Updated report

---
+++ src/models/modules/supervised/feature_extractors/h1_optimus_feature_extractor.py
@@ -0,0 +1,149 @@
+# from .base_feature_extractor import BaseFeatureExtractor
+from huggingface_hub import login
+import timm
+
+from abc import ABC, abstractmethod
+import torch
+import torch.nn as nn
+
+
+class BaseFeatureExtractor(ABC, nn.Module):
+    """
+    Base class for all feature extractors.
+    All feature extractors should inherit from this class.
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        output_size: int,
+        freeze: bool = False,
+        unfreeze_last_n_blocks: int = 0,
+        unfreeze_after_n_epochs: int = 0,
+    ):
+        super().__init__()
+        self.output_size = output_size
+        self.freeze = freeze
+        self.unfreeze_last_n_blocks = unfreeze_last_n_blocks
+        self.unfreeze_after_n_epochs = unfreeze_after_n_epochs
+        self.input_dim = input_dim
+        self.feature_extractor = None
+        self.frozen = freeze
+        self.float()
+
+    def post_init(self) -> None:
+        """
+        Post-initialization hook.
+        """
+        if self.freeze:
+            for param in self.feature_extractor.parameters():
+                param.requires_grad = False
+
+    def freeze_feature_extractor(self) -> None:
+        """
+        Freeze the feature extractor.
+        """
+        self.frozen = True
+        for param in self.feature_extractor.parameters():
+            param.requires_grad = False
+
+    def check_for_unfreezing(self, current_epoch: int) -> None:
+        """
+        Check if the feature extractor should be unfreezed.
+        """
+        if (
+            self.frozen
+            and self.unfreeze_after_n_epochs > 0
+            and current_epoch >= self.unfreeze_after_n_epochs
+        ):
+            print(f"Unfreezing feature extractor at epoch {current_epoch}")
+            self.frozen = False
+            c = 0
+            for layer in list(self.feature_extractor.children())[::-1]:
+                if (
+                    isinstance(layer, nn.Linear)
+                    or isinstance(layer, nn.Conv2d)
+                    or isinstance(layer, nn.Sequential)
+                ):
+                    c += 1
+                    if c > self.unfreeze_last_n_blocks:
+                        break
+                    for param in layer.parameters():
+                        param.requires_grad = True
+
+    @abstractmethod
+    def forward(self, x_image: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the feature extractor.
+
+        Args:
+            x_image: Image tensor of shape (batch_size, channels, height, width)
+
+        Returns:
+            Extracted features tensor
+        """
+
+    # def get_output_size(self) -> int:
+    #     """
+    #     Get the flattened output feature dimension of the extractor.
+
+    #     Returns:
+    #         Integer number of features after the extractor's forward pass.
+    #     """
+    #     with torch.no_grad():
+    #         dummy = torch.zeros(
+    #             size=(1, 3, self.input_dim, self.input_dim), dtype=torch.float32
+    #         )
+    #         output = self.forward(dummy)
+    #         if output.dim() > 2:
+    #             output = output.view(output.size(0), -1)
+    #         return int(output.shape[1])
+
+    @staticmethod
+    def create_feature_extractor(
+        feature_extractor_name: str, input_dim: int, feature_extractor_config: dict
+    ) -> "BaseFeatureExtractor":
+        if feature_extractor_name == "simple_cnn":
+            from .simple_cnn_feature_extractor import SimpleCNNFeatureExtractor
+
+            return SimpleCNNFeatureExtractor(input_dim, **feature_extractor_config)
+        elif feature_extractor_name.startswith("resnet"):
+            from .resnet_feature_extractor import ResNetFeatureExtractor
+
+            return ResNetFeatureExtractor(
+                input_dim, model_name=feature_extractor_name, **feature_extractor_config
+            )
+        else:
+            raise ValueError(f"Feature extractor {feature_extractor_name} not found")
+
+    def to_dict(self) -> dict:
+        """
+        Convert the feature extractor to a dictionary.
+        """
+        return {
+            "input_dim": self.input_dim,
+            "output_size": self.output_size,
+            "freeze": self.freeze,
+            "unfreeze_after_n_epochs": self.unfreeze_after_n_epochs,
+            "unfreeze_last_n_blocks": self.unfreeze_last_n_blocks,
+            "feature_extractor": str(self.feature_extractor),
+        }
+
+
+class H1OptimusFeatureExtractor(BaseFeatureExtractor):
+    def __init__(self, input_dim: int, freeze: bool = False, unfreeze_last_n_blocks: int = 0, unfreeze_after_n_epochs: int = 0):
+        super().__init__(input_dim, 1536, freeze, unfreeze_last_n_blocks, unfreeze_after_n_epochs)
+
+        login()
+        model = timm.create_model(
+            "hf-hub:bioptimus/H-optimus-1", pretrained=True, init_values=1e-5, dynamic_img_size=False
+        )
+        self.feature_extractor = model
+        self.post_init()
+
+    def forward(self, x):
+        return self.feature_extractor(x)
+
+if __name__ == "__main__":
+    feature_extractor = H1OptimusFeatureExtractor(input_dim=3, output_size=128)
+    print(feature_extractor.feature_extractor)
